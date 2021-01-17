f889f7ed26d9f6a5eab2f356549a13c95c694e6a
Boris Yevstyhneiev <molnerats@gmail.com>
2021-01-17 14:53:35 +0200

    Initial commit

---
+++ app/src/main/java/com/wikia/meownjik/pointstransformation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wikia.meownjik.pointstransformation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
